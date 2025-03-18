ebd7f0b75dea785a980075915fbf6b304dd05c62
na4ma4 <na4ma4@users.noreply.github.com>
2025-03-18 11:51:18 +1000

    feat: Updated to cliversion and fixed Goreleaser config

--- cmd/docker-pull-output/main.go
+++ cmd/docker-pull-output/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"os"
 
+	"github.com/dosquad/go-cliversion"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "docker-pull-ci",
-	Short: "Command to parse output of docker pull and docker push for readability on long push/pulls",
-	Run:   parseCommand,
-	Args:  cobra.NoArgs,
+	Use:     "docker-pull-ci",
+	Short:   "Command to parse output of docker pull and docker push for readability on long push/pulls",
+	Run:     parseCommand,
+	Args:    cobra.NoArgs,
+	Version: cliversion.Get().VersionString(),
 }
 
 func init() {
